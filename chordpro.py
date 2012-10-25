7d7f2927e020a64e4604319d812c397cd1660718
Benedikt Sauer <filmor@gmail.com>
2012-10-25 07:20:36 +0200

    Parse long directives.

--- chordpro.py
+++ chordpro.py
@@ -1,68 +1,11 @@
 #!/usr/bin/env python
 """ChordPro Parser"""
 
+from parser import *
 from ascii_visitor import *
 from chordpro_visitor import *
 from transpose_visitor import *
 from xml_visitor import *
-
-def my_partition(string, sep):
-    a, b, c = string.partition(sep)
-    return (a, b + c)
-
-def parse_line(string):
-    line = ""
-    chords = []
-    string = string.strip()
-    if string.startswith('#'):
-        return ("comment", string[1:])
-    if string.startswith('{'):
-        return ("command",) + tuple(string.strip('{} ').split(":", 1))
-    if not string.startswith('['):
-        start, string = my_partition(string, '[')
-        chords.append((None, start))
-    while string:
-        before, _, after = string.partition(']')
-        if not before.startswith('['):
-            raise Exception("Aye")
-        _, _, chord = before.partition('[')
-        line, string = my_partition(after, '[')
-        chords.append((chord, line))
-    return ("line", chords) if chords != [(None, '')] else ("command", "nl",)
-
-def parse(stream):
-    res = []
-    for i in stream.readlines():
-        parsed = parse_line(i.rstrip())
-        if parsed:
-            res.append(parsed)
-    return res
-
-def dispatch(parsed, visitor):
-    def visit(flag, *args):
-        f = lambda *args2: None
-        if hasattr(visitor, "visit_" + flag):
-            f = getattr(visitor, "visit_" + flag)
-        elif hasattr(visitor, "visit"):
-            f = lambda *args2: visitor.visit(flag, *args2)
-        return f(*args)
-
-    for el in parsed:
-        type = el[0]
-        val = el[1:]
-        if type == "command":
-            visit(val[0], *val[1:])
-        elif type == "comment":
-            visit("comment", *val)
-        elif type == "line":
-            visit("line", *val)
-        else:
-            visit(type, *val)
-
-def show(parsed, visitor_class, *args):
-    visitor = visitor_class(*args)
-    dispatch(parsed, visitor)
-    return visitor.result()
 
 if __name__ == '__main__':
     import codecs, argparse
---
+++ parser.py
@@ -0,0 +1,75 @@
+__all__ = ["parse", "dispatch", "show"]
+
+def my_partition(string, sep):
+    a, b, c = string.partition(sep)
+    return (a, b + c)
+
+shorten_map = {
+    "title": "t",
+    "subtitle": "st",
+    "comment": "c",
+    "define": "d",
+    "start_of_chorus": "soc",
+    "end_of_chorus": "eoc",
+    "start_of_tab": "sot",
+    "end_of_tab": "eot"
+}
+
+def parse_line(string):
+    line = ""
+    chords = []
+    string = string.strip()
+    if string.startswith('#'):
+        return ("comment", string[1:])
+    if string.startswith('{'):
+        s = string.strip('{} ').split(":", 1)
+        directive = s[0]
+        args = tuple(s[1].strip("{} ").split(":")) if len(s) == 2 else ()
+        directive = shorten_map.get(directive, directive)
+        return ("directive", directive) + args
+    if not string.startswith('['):
+        start, string = my_partition(string, '[')
+        chords.append((None, start))
+    while string:
+        before, _, after = string.partition(']')
+        if not before.startswith('['):
+            raise Exception("Aye")
+        _, _, chord = before.partition('[')
+        line, string = my_partition(after, '[')
+        chords.append((chord, line))
+    return ("line", chords) if chords != [(None, '')] else ("directive", "nl",)
+
+def parse(stream):
+    res = []
+    for i in stream.readlines():
+        parsed = parse_line(i.rstrip())
+        if parsed:
+            res.append(parsed)
+    return res
+
+def dispatch(parsed, visitor):
+    def visit(flag, *args):
+        f = lambda *args2: None
+        if hasattr(visitor, "visit_" + flag):
+            f = getattr(visitor, "visit_" + flag)
+        elif hasattr(visitor, "visit"):
+            f = lambda *args2: visitor.visit(flag, *args2)
+        return f(*args)
+
+    for el in parsed:
+        type = el[0]
+        val = el[1:]
+        if type == "directive":
+            visit(val[0], *val[1:])
+        elif type == "comment":
+            visit("comment", *val)
+        elif type == "line":
+            visit("line", *val)
+        else:
+            visit(type, *val)
+
+def show(parsed, visitor_class, *args):
+    visitor = visitor_class(*args)
+    dispatch(parsed, visitor)
+    return visitor.result()
+
